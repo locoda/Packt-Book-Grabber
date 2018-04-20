27323249571784c367a15676de4fbee5132b77f5
He Huang <hhuang12006@gmail.com>
2018-04-20 00:47:58 -0400

    add ifttt notification support

--- script.py
+++ script.py
@@ -1,15 +1,16 @@
 import json
-from lxml import html
 
 import mechanicalsoup
+import requests
+from lxml import html
 
 BASE_URL = "https://www.packtpub.com"
 LOGIN_URL = "https://www.packtpub.com/login"
 FREE_BOOk_URL = "https://www.packtpub.com/packt/offers/free-learning"
 MY_EBOOK_URL = "https://www.packtpub.com/account/my-ebooks"
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 
-
-def login(file, browser):
+def login(browser, file='credential.json'):
     """
     login to Packt
         :param file: json file contains username and password
@@ -36,12 +37,22 @@ def claim_book(browser):
     return ('account' in browser.get_url())
 
 
+def ifttt_notify(file='credential.json'):
+    r = requests.get(FREE_BOOk_URL, headers={'User-Agent': USER_AGENT})
+    t = html.fromstring(r.content)
+    with open('credential.json') as fd:
+        data = json.load(fd)
+    title = "".join(t.xpath('//div[@class="dotd-title"]//text()')).strip()
+    r = requests.post(data["ifttt"], data = { "value1" : title})
+
+
 browser = mechanicalsoup.StatefulBrowser(
     soup_config={'features': 'lxml'},
-    user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    user_agent=USER_AGENT
 )
 
-if not login('credential.json', browser):
+if not login(browser):
     print('Login Failed.. Check your "credential.json" file')
 if not claim_book(browser):
     print('Claim Failed.. Not sure Why')
+# ifttt_notify()
