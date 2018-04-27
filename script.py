8811f756e1b0e174ec8b03eede3b3f329f2982f1
He Huang <hhuang12006@gmail.com>
2018-04-27 14:17:59 -0400

    add mailgun support

--- script.py
+++ script.py
@@ -14,11 +14,12 @@ MY_EBOOK_URL = "https://www.packtpub.com/account/my-ebooks"
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 headers = {'User-Agent': USER_AGENT}
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--claim", action="store_true",
                         help="if claim free book on this run")
-    parser.add_argument("-n", "--notify", type=str, choices=["ifttt"],
+    parser.add_argument("-n", "--notify", type=str, choices=["ifttt", "mailgun"],
                         help="notify the selected agent")
     parser.add_argument("-d", "--download", type=int,
                         help="number of book to download")
@@ -27,6 +28,7 @@ def parse_arguments():
     parser.add_argument("--dir", type=str, default="./",
                         help="direcotry you want to download the book, end with /, default as current directory")
     return parser.parse_args()
+
 
 def login(s, file='credential.json'):
     """
@@ -99,11 +101,11 @@ def download_book(s, n, dtype, ddir):
     for book in booklist[:n]:
         nid = book.xpath('./@nid')[0]
         title = book.xpath('./@title')[0]
-        print("Downloading Book " + nid + ": " + title)
+        print("Downloading Book %s: %s" % (nid, title))
         try:
-            link = book.xpath('.//a[contains(@href,"' + dtype + '")]/@href')[0]
+            link = book.xpath('.//a[contains(@href,"%s")]/@href' % dtype)[0]
         except IndexError:
-            print(dtype + " for this book doesn't exsit")
+            print("%s for this book doesn't exsit" % dtype)
             continue
         r = s.get(BASE_URL + link, headers=headers, stream=True)
         filename = ddir + title + "." + dtype
@@ -111,41 +113,84 @@ def download_book(s, n, dtype, ddir):
             shutil.copyfileobj(r.raw, f)
 
 
+def _get_free_book_title():
+    """
+    get title of today's free book -- used for sent notifications
+    """
+    r = requests.get(FREE_BOOk_URL, headers={'User-Agent': USER_AGENT})
+    t = html.fromstring(r.content)
+    return ("".join(t.xpath('//div[@class="dotd-title"]//text()')).strip())
+
+
 def ifttt_notify(file='credential.json'):
     """
     IFTTT notification
         :param file:  contains ifttt webhook
     """
-    r = requests.get(FREE_BOOk_URL, headers={'User-Agent': USER_AGENT})
-    t = html.fromstring(r.content)
+
     with open(file) as fd:
         data = json.load(fd)
-    title = "".join(t.xpath('//div[@class="dotd-title"]//text()')).strip()
-    print("Notify with booktitle: " + title)
+
     try:
-        r = requests.post(data["ifttt"], data={"value1": title})
+        r = requests.post(data["ifttt"], data={
+                          "value1": _get_free_book_title()})
     except KeyError:
         print('Please add your ifttt webhook in "credential.json" file')
+    return (r.status_code is 200)
+
+
+def mailgun_notify(file='credential.json'):
+    """
+    Mailgun notification
+        :param file:  contains mailgup domain, api and email address
+    """
+    with open(file) as fd:
+        data = json.load(fd)
+
+    r = requests.post(
+        "https://api.mailgun.net/v3/%s/messages" % data['mailgun']['domain'],
+        auth=("api", data['mailgun']['api']),
+        data={"from": "PacktPub Notification <packtpub@%s>" % data['mailgun']['domain'],
+              "to": "<%s>" % data['mailgun']['to'],
+              "subject": "Today's Free book from PacktPub!",
+              "text": "Today's free book is %s" % _get_free_book_title()})
+
+    return (r.status_code is 200)
+
 
 if __name__ == "__main__":
     # parse arguments
     args = parse_arguments()
     # create a requests session using through process
     s = requests.Session()
-    # Login
-    if not login(s):
-        print('Login Failed.. Check your username and password in "credential.json" file')
-    else:
-        print('Login Successful!!')
-    # Check if claim
-    if args.claim:
-        if not claim_book(s):
-            print('Claim Failed.. Check your anti-captcha in "credential.json" file')
 
     # Check if should notify
     if args.notify is not None:
         if args.notify == "ifttt":
-            ifttt_notify()
-    # Check if should download
-    if args.download is not None:
-        download_book(s, args.download, args.type, args.dir)
+            if ifttt_notify():
+                print("ifttt successful notified")
+            else:
+                print("ifttt failed")
+        if args.notify == "mailgun":
+            if mailgun_notify():
+                print("mailgun successful notified")
+            else:
+                print("mailgun failed")
+
+    if (args.claim or args.download):
+        # Login
+        if login(s):
+            print('Login Successful!!')
+        else:
+            print('Login Failed.. Check your username and password in "credential.json" file')
+
+        # Check if claim
+        if args.claim:
+            if claim_book(s):
+                print('Claim Successful')
+            else:
+                print('Claim Failed.. Check your anti-captcha in "credential.json" file')
+
+        # Check if should download
+        if args.download is not None:
+            download_book(s, args.download, args.type, args.dir)
