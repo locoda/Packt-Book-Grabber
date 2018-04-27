6938387481fcdb679358413a30f2185935e021a4
He Huang <hhuang12006@gmail.com>
2018-04-26 23:23:26 -0400

    add downloading options

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import shutil
 
 import requests
 from lxml import html
@@ -12,11 +13,11 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36
 headers = {'User-Agent': USER_AGENT}
 
 
-def login(session, file='credential.json'):
+def login(s, file='credential.json'):
     """
     login to Packt
+        :param s:  session used cross file
         :param file: json file contains username and password
-        :param browser:  browser used cross file
     """
     r = s.get(LOGIN_URL, headers=headers)
     tree = html.fromstring(r.content)
@@ -33,22 +34,52 @@ def login(session, file='credential.json'):
     }
     data['op'] = "Log in"
     data['form_build_id'] = tree.xpath(
-        '//form[@id="packt-v3-account-login-form"]//input[@name="form_build_id"]/@value')
+        '//form[@id="packt-v3-account-login-form"]//input[@name="form_build_id"]/@value')[0]
     data['form_id'] = tree.xpath(
-        '//form[@id="packt-v3-account-login-form"]//input[@name="form_id"]/@value')
-
+        '//form[@id="packt-v3-account-login-form"]//input[@name="form_id"]/@value')[0]
     r = s.post(LOGIN_URL, data=data, headers=headers)
     return ('account' in r.url)
 
 
-def claim_book(session):
+def claim_book(s):
     """
     claim the free book
-        :param browser:  browser used cross file
+        :param s:  session used cross file
     """
 
+def download_book(s, n, dtype, ddir):
+    """
+    Download books
+        :param s:  session used cross file
+        :param n: max number of books to download
+        :param dtype: type of books to download
+        :param ddir: directory of books to download
+    """
+    r = s.get(MY_EBOOK_URL, headers=headers)
+    tree = html.fromstring(r.content)
+
+    booklist = tree.xpath('//div[@id="product-account-list"]/div[@nid]')
+    n = min(len(booklist), n)
+    for book in booklist[:n]:
+        nid = book.xpath('./@nid')[0]
+        title = book.xpath('./@title')[0]
+        print("Downloading Book " + nid + ": " + title )
+        try:
+            link = book.xpath('.//a[contains(@href,"' + dtype + '")]/@href')[0]
+        except IndexError:
+            print(dtype + " for this book doesn't exsit")
+            continue
+        print(link)
+        r = s.get(BASE_URL + link, headers=headers, stream=True)
+        filename = ddir + title + "." + dtype
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
 
 def ifttt_notify(file='credential.json'):
+    """
+    IFTTT notification
+        :param file:  contains ifttt webhook
+    """
     r = requests.get(FREE_BOOk_URL, headers={'User-Agent': USER_AGENT})
     t = html.fromstring(r.content)
     with open(file) as fd:
@@ -61,13 +92,26 @@ def ifttt_notify(file='credential.json'):
         print('Please add your ifttt webhook in "credential.json" file')
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--notify", type=str,
-                    help="notify the selected agent: e.g. ifttt")
+parser.add_argument("-n", "--notify", type=str, choices=["ifttt"],
+                    help="notify the selected agent")
+parser.add_argument("-d", "--download", type=int,
+                    help="number of book to download")
+parser.add_argument("-t", "--type", type=str, choices=["pdf", "epub"], default="pdf",
+                    help="type of book to download, default as pdf")
+parser.add_argument("--dir", type=str, default="./",
+                    help="direcotry you want to download the book, end with /")
 args = parser.parse_args()
-s = requests.Session()
 
+s = requests.Session()
+# Login
 if not login(s):
     print('Login Failed.. Check your "credential.json" file')
-print(args.notify)
-if args.notify == "ifttt":
-    ifttt_notify()
+
+# Check if should notify 
+if args.notify is not None:
+    if args.notify == "ifttt":
+        ifttt_notify()
+
+# Check if should download
+if args.download is not None:
+    download_book(s, args.download, args.type, args.dir)
